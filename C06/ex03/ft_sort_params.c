329da8db93e7380a00d1eb0c7281ad0c17a27e22
victor <victormorenomorillo@gmail.com>
2025-02-23 17:42:37 +0100

    Added python to install Norminette & fixed C06 ex03

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victmore <victmore@student.42.fr>          +#+  +:+       +#+        */
+/*   By: victor <victor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:34:52 by victmore          #+#    #+#             */
-/*   Updated: 2025/02/19 19:33:12 by victmore         ###   ########.fr       */
+/*   Updated: 2025/02/23 17:40:30 by victor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,27 +34,27 @@ int	ft_strcmp(char *s1, char *s2)
 	return (*s1 - *s2);
 }
 
-void	ft_swap(int *a, int *b)
+void	ft_swap(char **a, char **b)
 {
-	int	temp;
+	char	*temp;
 
 	temp = *a;
 	*a = *b;
 	*b = temp;
 }
 
-void	ft_sort(int *argv, int argc)
+void	ft_sort(char **argv, int argc)
 {
 	int	i;
 	int	curr;
 
-	i = 0;
-	while (i < size)
+	i = 1;
+	while (i < argc)
 	{
 		curr = i;
-		while (curr > 0 && tab[curr] > tab[curr - 1])
+		while (curr > 1 && ft_strcmp(argv[curr - 1], argv[curr]) > 0)
 		{
-			ft_swap(&tab[curr], &tab[curr - 1]);
+			ft_swap(&argv[curr], &argv[curr - 1]);
 			curr--;
 		}
 		i++;
@@ -65,7 +65,7 @@ int	main(int argc, char **argv)
 {
 	int	i;
 
-	ft_sort(argv, argc - 1);
+	ft_sort(argv, argc);
 	i = 1;
 	while (i < argc)
 	{
