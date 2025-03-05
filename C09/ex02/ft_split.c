584db17ef92f3a1d9eff1aa7bc13e7c46a38ef33
victor <victormorenomorillo@gmail.com>
2025-03-05 14:29:23 +0100

    Remade ft_split.c in C09

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -2,166 +2,117 @@
 
 int	is_sep(char c, char *charset)
 {
-	while (*charset)
+	int	i;
+
+	i = 0;
+	while (charset[i])
 	{
-		if (c == *charset)
+		if (c == charset[i])
 			return (1);
-		charset++;
+		i++;
 	}
 	return (0);
 }
 
-int	ft_count_sep_words(char *str, char *charset)
+int	count_words(char *str, char *charset)
 {
-	int	words;
 	int	in_word;
+	int	i;
+	int	words;
 
-	words = 0;
 	in_word = 0;
-	while (*str)
+	i = 0;
+	words = 0;
+	while (str[i])
 	{
-		if (!is_sep(*str, charset) && !in_word)
+		if (!in_word && !is_sep(str[i], charset))
 		{
-			words++;
 			in_word = 1;
+			words++;
 		}
-		else if (is_sep(*str, charset) && in_word)
+		else if (in_word && is_sep(str[i], charset))
+		{
 			in_word = 0;
-		str++;
+		}
+		i++;
 	}
 	return (words);
 }
 
-char	*retrieve_word(char *start, char *end)
+char	*word_malloc(char *start, char *end)
 {
 	char	*ptr;
+	int		len;
 	int		i;
 
-	ptr = malloc((sizeof(char) * (end - start)) + 1);
-	if (!ptr)
-		return (NULL);
+	len = end - start + 1;
+	ptr = malloc((sizeof(char) * len) + 1);
 	i = 0;
-	while (start < end)
-		ptr[i++] = *start++;
-	ptr[i] = '\0';
+	while (i < len)
+	{
+		ptr[i] = start[i];
+		i++;
+	}
 	return (ptr);
 }
 
-char	*next_word_pointer(int i, char *str, char *charset)
+char	*get_word_in_index(char *str, char *charset, int index)
 {
-	int		words;
-	int		in_word;
 	char	*start;
+	int		in_word;
+	int		words;
 
-	words = 0;
 	in_word = 0;
-	while (*str)
+	words = 0;
+	while (str)
 	{
-		if (!is_sep(*str, charset) && !in_word)
+		if (!in_word && !is_sep(*str, charset))
 		{
 			words++;
-			in_word = 1;
 			start = str;
+			in_word = 1;
 		}
-		if ((is_sep(*str, charset) || *(str + 1) == '\0') && in_word)
+		if (in_word && (is_sep(*str, charset) || *(str + 1) == '\0'))
 		{
 			in_word = 0;
-			if (words - 1 == i && is_sep(*str, charset))
-				return (retrieve_word(start, str));
-			else if (words - 1 == i)
-				return (retrieve_word(start, str + 1));
+			if ((words - 1 == index) && (*(str + 1) == '\0'))
+				return (word_malloc(start, str));
+			else if (words - 1 == index)
+				return (word_malloc(start, str - 1));
 		}
 		str++;
 	}
-	return (NULL);
+	return ('\0');
 }
 
 char	**ft_split(char *str, char *charset)
 {
-	char	**solution;
-	int		word_num;
+	char	**sol;
+	int		words;
 	int		i;
 
+	words = count_words(str, charset);
+	sol = malloc(sizeof(char *) * words);
 	i = 0;
-	word_num = ft_count_sep_words(str, charset);
-	solution = malloc((sizeof(void *) * word_num) + 1);
-	if (!solution)
-		return (NULL);
-	while (i < word_num)
+	while (i < words)
 	{
-		solution[i] = next_word_pointer(i, str, charset);
+		sol[i] = get_word_in_index(str, charset, i);
 		i++;
 	}
-	solution[i] = '\0';
-	return (solution);
+	sol[i] = '\0';
+	return (sol);
 }
 
 // #include <stdio.h>
 // int main(void)
 // {
-//     char *str = " -Hola-:;;-:-mundo,esto;es-una:prueba;;;; ";
-//     char *charset = "-,;:";
-//     char **result = ft_split(str, charset);
-//     for (int i = 0; result[i-1] != 0; i++)
+// 	char *str = " -Hola-:;;-:-mundo,esto;es-una:prueba;;;; - vic 	";
+// 	char *charset = "-,;:";
+// 	char **result = ft_split(str, charset);
+// 	for (int i = 0; result[i - 1] != 0; i++)
 // 	{
-//         printf("result[%d]: \"%s\"\n", i, result[i]);
+// 		printf("result[%d]: \"%s\"\n", i, result[i]);
 // 	}
 // 	free (result);
 //     return (0);
-// }
-
-// #include <stdio.h>
-// int main(void)
-// {
-//     printf("Size of char: %zu bytes\n", sizeof(char));
-//     printf("Size of int: %zu bytes\n", sizeof(int));
-//     printf("Size of pointer: %zu bytes\n", sizeof(void *));
-//     return 0;
-// }
-
-// int	count_sep_words(char *str, char *charset)
-// {
-// 	int		charset_len;
-// 	int		words;
-// 	int		i;
-// 	int		j;
-// 	int		k;
-// 	int		l;
-// 	charset_len = ft_strlen(charset);
-// 	words = 0;
-// 	i = 0;
-// 	while (str[i])
-// 	{
-// 		j = 0;
-// 		l = 0;
-// 		while (j < charset_len)
-// 		{
-// 			if (str[i] == charset[j])
-// 			{
-// 				k = 0;
-// 				l = 0;
-// 				while (k < charset_len)
-// 				{
-// 					if (str[i + 1] != charset[k])
-// 						l++;
-// 					if (l == charset_len)
-// 					{
-// 						words++;
-// 						l = 0;
-// 					}
-// 					k++;
-// 				}
-// 			}
-// 			else if (i == 0)
-// 				l++;
-// 			if (l == charset_len)
-// 			{
-// 				words++;
-// 				l = 0;
-// 			}
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// 	return (words);
 // }
